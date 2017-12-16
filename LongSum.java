ffea7ac99857ff3ad96ed770788a197560ed015a
trhoades259 <trhoades259@gmail.com>
2017-12-16 13:23:41 -0600

    longsum with arraylists

--- LongSum.java
+++ LongSum.java
@@ -5,24 +5,20 @@ class LongSum {
 		EasyReader l = new EasyReader(), in = new EasyReader(l.readLine());
 		System.out.println("Span of Numbers?: ");
 		int i=0,t=0,g=0,s=l.readInt();
-		int[] n = new int[500], a = new int[s], b = new int[s];
-		n[i]=in.readInt();
-		while (!in.eof()&&i<499) {
-			i++;
-			n[i]=in.readInt();
-		}
+		ArrayList<Integer> n = new ArrayList<Integer>(), a = new ArrayList<Integer>(), b = new ArrayList<Integer>();
+		for (;!in.eof()&&i<500; i++) n.add(in.readInt());
 		for (int o=s-1; o < 500; o++) {
-			for (int y = 0; y < s; y++) {
-				t+=n[o-y];
-			}
+			for (int y = 0; y < s; y++) t+=n.get(o-y);
 			if(t>g) {
 				g=t;
-				for (int m = 0; m < s; m++) a[m]=o-(s-2)+m;
-				for (int v = 0; v < s; v++) b[v]=n[o-(s-1)+v];
+				a.clear();
+				b.clear();
+				for (int m = 0; m < s; m++) a.add(o-(s-2)+m);
+				for (int v = 0; v < s; v++) b.add(n.get(o-(s-1)+v));
 			}
 			t=0;
 		}
 		System.out.println("Largest Sum Over Span "+s+": "+g);
-		for(int u = 0; u < s; u++) System.out.println(a[u]+": "+b[u]);
+		for(int u = 0; u < s; u++) System.out.println(a.get(u)+": "+b.get(u));
 	}
 }
--- Riech.java
+++ Riech.java
@@ -2,7 +2,7 @@ import java.util.*;
 class Riech {
 	public static void main(String [] args) {
 		Random r = new Random();
-		EasyWriter out = new EasyWriter("MeinKampf.txt");
+		EasyWriter out = new EasyWriter("random.txt");
 		int n;
 		for (int h=0; h < 500; h++) {
 			n=r.nextInt(100)+1;
